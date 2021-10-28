f7b24bfe3a0a288320d88248e0830fd42aafaeaf
hoang <hoangdohuy29092002@gmail.com>
2021-10-28 20:27:26 +0700

    abc

---
+++ src/model/PostAudience.java
@@ -0,0 +1,28 @@
+package model;
+
+public enum PostAudience  {
+    PUBLIC(0, "Public", "images/ic_public.png"),
+    FRIENDS(1, "Friend", "images/ic_friend.png");
+
+    private int id;
+    private String name;
+    private String imgSrc;
+
+    PostAudience(int id, String name, String imgSrc) {
+        this.id = id;
+        this.name = name;
+        this.imgSrc = imgSrc;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+}
